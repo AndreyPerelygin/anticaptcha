a66060fc61ad14c44fa37a9ecf5aa7319898aa89
Andrey <andrey@zencore.org>
2016-12-04 16:20:25 +0300

    no proxy

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 
 setup(name='anticaptcha',
       version=version,
-      description="0.1",
+      description="0.2",
       long_description="""\
 Python SDK for API 2.0 anti-captcha.com""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
